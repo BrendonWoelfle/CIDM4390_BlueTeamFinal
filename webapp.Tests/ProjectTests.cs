905e5cfe03ba46e73fd977189c3769f493048685
Brendon <bmwoelfle1@buffs.wtamu.edu>
2022-04-23 11:22:14 -0500

    added Tests files

---
+++ webapp.Tests/ProjectTests.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using WebApp1.Data;
+using WebApp1.Models;
+
+namespace webapp.Tests;
+
+public class ProgramAdminTest
+{
+    
+}
