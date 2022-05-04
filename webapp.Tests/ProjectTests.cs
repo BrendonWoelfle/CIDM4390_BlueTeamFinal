d0c00a105ee435c3a6b842e877b737fca4b237c4
Brendon <bmwoelfle1@buffs.wtamu.edu>
2022-05-03 20:13:29 -0500

    Linked models 2

--- webapp.Tests/ProjectTests.cs
+++ webapp.Tests/ProjectTests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
-using WebApp1.Data;
+//using WebApp1.Data;
 using WebApp1.Models;
 
 namespace webapp.Tests;
