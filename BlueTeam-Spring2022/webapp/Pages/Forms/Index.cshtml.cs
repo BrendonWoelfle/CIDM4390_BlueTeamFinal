77fa52f922dc83f5830e638737be27aea0abd6d0
Brendon <bmwoelfle1@buffs.wtamu.edu>
2022-05-04 21:14:55 -0500

    Added Form + Authorization

--- BlueTeam-Spring2022/webapp/Pages/Forms/Index.cshtml.cs
+++ BlueTeam-Spring2022/webapp/Pages/Forms/Index.cshtml.cs
@@ -5,26 +5,72 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using webapp.Data;
-using webapp.Models;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;        // Required using directives for Data Validation
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Authorization;
 
 namespace webapp.Pages.Forms
 {
+    [Authorize]
     public class IndexModel : PageModel
-    {
-        private readonly webapp.Data.ApplicationDbContext _context;
+{
+    [BindProperty]
+        // Add Data Validation Rules
+        [Display(Name = "First Name")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string FirstName {get; set;}
 
-        public IndexModel(webapp.Data.ApplicationDbContext context)
+        [BindProperty]
+        public string LastName {get; set;}
+
+        [BindProperty]
+        // Add Data Validation Rules
+        [Display(Name = "Course")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string CouseName {get; set;}
+
+        [BindProperty]
+        public string Semester {get; set;}
+
+        [BindProperty]
+        public int Year {get; set;}
+
+        [BindProperty]
+        public string CourseCoordinator {get;set;}
+        [BindProperty]
+        public string CatalogDescription {get;set;}
+        [BindProperty]
+        public string GradeDistrubution {get;set;}
+        [BindProperty]
+        public string ModificationsCourse {get;set;}
+        [BindProperty]
+        public string StudentFeedback {get;set;}
+        [BindProperty]
+        public string Reflection {get;set;}
+        [BindProperty]
+        public string CourseImprovement {get;set;}
+
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
         {
-            _context = context;
+            _logger = logger;
         }
 
-        public IList<Form> Form { get;set; }
-
-        public async Task OnGetAsync()
+        public void OnGet()
         {
-            Form = await _context.Form.ToListAsync();
+                
+        }
+
+        public void OnPost()
+        {
+            // Print out FirstName and LastName just like any other property.
+            _logger.LogWarning($"On Post {FirstName} {LastName}");
         }
     }
 }
